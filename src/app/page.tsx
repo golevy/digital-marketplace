d3e7ef3dae421582c8f4c6122c687db06964e9d0
Levy <golvwei@gmail.com>
2024-04-13 21:51:35 +0800

    feat: Add max width wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import MaxWidthWrapper from "~/components/MaxWidthWrapper";
+
 export default function Home() {
-  return <p>hello world</p>;
+  return (
+    <MaxWidthWrapper>
+      <div className="mx-auto flex max-w-3xl flex-col items-center py-20 text-center">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Your marketplace for high-quality{" "}
+          <span className="text-blue-600">digital assets</span>.
+        </h1>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
